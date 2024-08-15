263b048e55affeff56219474442124e357fa95b1
Gabryel <gabrielvenanciocleffs@gmail.com>
2024-08-15 19:58:25 -0300

    tipos de dados

---
+++ tipos.js
@@ -0,0 +1,13 @@
+// Tipos de dados
+
+let a, b, c, d, e, f, g;
+
+a = undefined;
+b = null;
+c = 0;
+d = "olá mundo!";
+e = false;
+f = { x: 10 };
+g = [1, 2, 3];
+
+// mostrar no console utilizando o console.log
