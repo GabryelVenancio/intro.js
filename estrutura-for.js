e249f3d4f98f6c88fc5d6494cdd6ead30952d598
Gabryel <gabrielvenanciocleffs@gmail.com>
2024-08-15 20:21:21 -0300

    FOR

--- estrutura-for.js
+++ estrutura-for.js
@@ -4,6 +4,20 @@ const n = x.length;
 
 console.log("len array:", n);
 
+// for tradicional
+
 for (let i = 0; i < n && i !== 2; i++) {
     console.log(`indice $(i) tem o valor ${x[i]}`)
+}
+
+// for in
+
+for (let m in y) {
+    console.log(m);
+}
+
+// for of
+
+for (let n of y) {
+    console.log(n);
 }
