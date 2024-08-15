6012ba3b447b3d94b7141b44be6a9f235a4a7ca9
Gabryel <gabrielvenanciocleffs@gmail.com>
2024-08-15 20:04:01 -0300

    IF

---
+++ estrutura-if.js
@@ -0,0 +1,21 @@
+let x = 10;
+
+if (x === 10) {
+    console.log("é igual a 10");
+}
+
+x = 5; 
+
+if (x == 10) {
+    console.log("é igual a 10");
+}
+else {
+    console.log("é diferente de 10");
+}
+
+if (x < 10) {
+    console.log("é menor que 10");
+}
+else if (x > 10) {
+    console.log("e maior que 10");
+}
